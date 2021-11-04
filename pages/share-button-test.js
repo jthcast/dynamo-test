cb1c9a4787404bdcd18d1ff5f2ca796b080c1067
jthcast <jthcast@gmail.com>
2021-11-04 09:45:57 +0900

    Fix unset url

--- pages/share-button-test.js
+++ pages/share-button-test.js
@@ -7,7 +7,6 @@ export default function ShareButtonTest() {
       <ShareButton 
         title='test title'
         text='test text!!'
-        url='http://192.168.0.16:3002/drops/market/0xb329ddcc6722c158c81BeDe6820dd419e6896486/1000010'
       />
     </>
   )
